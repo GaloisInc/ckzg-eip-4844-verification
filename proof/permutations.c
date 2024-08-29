279d17e22c3a5a6391b3b73709c09d9e93829ef6
Brett Decker <decker@galois.com>
2024-08-29 15:03:22 -0500

    proof: Add proof for c-kzg reverse_bits function
    It should be noted that this function does NOT match the Python
    specification, but it is used in the c-kzg implementation of
    `bit_reversal_permutation`, so we prove `reverse_bits` is equivalent
    to Cryptol's builtin `reverse` function.

--- proof/permutations.c
+++ proof/permutations.c
@@ -1,6 +1,17 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 bool is_power_of_two(uint64_t n) {
     return (n & (n - 1)) == 0;
+}
+
+uint64_t reverse_bits(uint64_t n) {
+    uint64_t result = 0;
+    for (size_t i = 0; i < 64; ++i) {
+        result <<= 1;
+        result |= (n & 1);
+        n >>= 1;
+    }
+    return result;
 }
